b63fcd636cc018f6ea00c650d7ee468396a1c65b
sebcheong <62134776+sebcheong@users.noreply.github.com>
2023-04-15 20:49:52 +0800

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,14 @@ from pprint import pprint
 
 
 if __name__ == "__main__":
+	# Initialise root window
 	root = Tk()
+	
+	# Setting window attributes
 	root.title("Query Comparer")
+	root.attributes('-fullscreen', True)
+	
+	# Constructing GUI
 	window = Window(root)
+	
 	root.mainloop()
